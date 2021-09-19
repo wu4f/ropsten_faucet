6bd5614901ec11404bbf66f957c571c3b86b685b
Wu-chang Feng <wuchang@pdx.edu>
2021-09-19 16:57:17 +0000

    dump individual entries

--- dump_users.py
+++ dump_users.py
@@ -3,7 +3,11 @@ import time
 conn = sqlite3.connect('./entries.db')
 cur = conn.cursor()
 cur.execute('SELECT * from users')
-print(cur.fetchall())
+res = cur.fetchall()
+for entry in res:
+    print(entry)
+exit()
+
 print(int(time.time()))
 cur.execute('UPDATE users set last=0 where email="wuchang@pdx.edu"')
 conn.commit()
