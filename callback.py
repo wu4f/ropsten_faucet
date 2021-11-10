5a47e0edfdd79919fa0471e176dfd9fc5ae6c9b4
Wu-chang Feng <wuchang@pdx.edu>
2021-11-10 19:02:14 +0000

    mobile UI

--- callback.py
+++ callback.py
@@ -5,11 +5,12 @@ import config
 
 class Callback(MethodView):
     def get(self):
-        google = OAuth2Session(config.client_id, redirect_uri = config.callback_url, state=session['oauth_state'])
-        token = google.fetch_token(config.token_url, client_secret=config.client_secret,
-                            authorization_response=request.url)
-        # At this point you can fetch protected resources but lets save
-        # the token and show how this is done from a persisted token
-        session['oauth_token'] = token
-
-        return redirect(url_for('eth'))
+        try:
+            google = OAuth2Session(config.client_id, redirect_uri = config.callback_url, state=session['oauth_state'])
+            token = google.fetch_token(config.token_url, client_secret=config.client_secret, authorization_response=request.url)
+            # At this point you can fetch protected resources but lets save
+            # the token and show how this is done from a persisted token
+            session['oauth_token'] = token
+            return redirect(url_for('eth'))
+        except:
+            return redirect(url_for('index'))
