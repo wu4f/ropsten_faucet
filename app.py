d1d5e4dd0d0af5f138e262079627eba0cfd10252
Wu-chang Feng <wuchang@pdx.edu>
2022-03-02 09:55:07 -0800

    add logging

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ A simple guestbook flask app.
 """
 import flask
 import os
+import logging
 from index import Index
 from eth import Eth
 from users import Users
 from callback import Callback
 from logout import Logout
+import config
+
+logging.basicConfig(filename=config.log_file, level=logging.INFO)
 
 app = flask.Flask(__name__)       # our Flask app
 app.secret_key = os.urandom(24)
@@ -33,6 +37,7 @@ app.add_url_rule('/users',
                  methods=["GET"])
 
 if __name__ == '__main__':
+    # Remove when deployed to production
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
     app.secret_key = os.urandom(24)
     app.run(host='0.0.0.0', port=8000, debug=True)
