b9b817a3e70aef83f1e7d67a311eda4800350ca0
Wu-chang Feng <wuchang@pdx.edu>
2022-03-13 07:02:14 -0700

    fix failing transaction bug in Issue 1

--- eth.py
+++ eth.py
@@ -9,110 +9,129 @@ import logging
 from datetime import datetime, timezone, timedelta
 
 class Eth(MethodView):
-    def get(self):
-        # If client has an OAuth2 token, use it to get their information and render
-        #   the eth page with it
-        if 'oauth_token' in session:
-            google = OAuth2Session(config.client_id, token=session['oauth_token'])
-            try:
-                userinfo = google.get('https://www.googleapis.com/oauth2/v3/userinfo').json()
-            except:
-                return redirect(url_for('logout'))
-
+    def validate_oauth(self, session):
+        google = OAuth2Session(config.client_id, token=session['oauth_token'])
+        try:
+            userinfo = google.get('https://www.googleapis.com/oauth2/v3/userinfo').json()
             email = userinfo['email']
-            if request.headers.getlist("X-Forwarded-For"):
-                ip = request.headers.getlist("X-Forwarded-For")[0]
-            else:
-                ip = request.remote_addr
-            m = model.get_model()
-            wait_time = int(time.time()) - m.select(email, ip, "y")
-            if wait_time < 1209600:
-                return render_template('eth.html', email=userinfo['email'], wait=wait_time)
-            else:
-                return render_template('eth.html', email=userinfo['email'])
+        except:
+            return ""
+        return email
+
+    def validate_ip(self, request):
+        if request.headers.getlist("X-Forwarded-For"):
+            return request.headers.getlist("X-Forwarded-For")[0]
         else:
-        # Redirect to the identity provider and ask the identity provider to return the client
-        #   back to /callback route with the code
+            return request.remote_addr
+
+    def validate_wallet(self, request):
+        if ('address' in request.form) and Web3.isAddress(request.form['address']):
+            return request.form['address']
+        return ""
+    
+    def calculate_eth(self, email, ip, wallet, last):
+        send_amount = 5.0
+        now = datetime.now().astimezone(timezone(timedelta(hours=-8))).strftime('%m-%d %H:%M')
+
+        if email.endswith('@pdx.edu'):
+            logging.info(f'{now}:Eth Max:{email}:{ip}:{wallet}')
+            send_amount = 10.0
+        
+        delta = int(time.time()) - last
+        if delta < 1209600:
+            now = datetime.now().astimezone(timezone(timedelta(hours=-8))).strftime('%m-%d %H:%M')
+            logging.info(f'{now}:Rate Limit:{email}:{ip}:{wallet}:{delta}')
+            send_amount = 0
+
+        return send_amount
+
+    def send_eth(self, email, ip, to_address, send_amount):
+        w3 = Web3(Web3.HTTPProvider(config.node_url))
+        address1 = Web3.toChecksumAddress(config.faucet_address)
+        address2 = Web3.toChecksumAddress(to_address)
+        to_balance = w3.eth.get_balance(address2)
+        if to_balance > 10000000000000000000:
+            now = datetime.now().astimezone(timezone(timedelta(hours=-8))).strftime('%m-%d %H:%M')
+            logging.info(f'{now}:High Balance:{email}:{ip}:{to_address}')
+            return 0
+        nonce = w3.eth.getTransactionCount(address1)
+        tx = {
+                'nonce': nonce,
+                'to': address2,
+                'value': w3.toWei(send_amount, 'ether'),
+                'gas': 21000,
+                'maxFeePerGas': w3.toWei(1000.0, 'gwei'),
+                'maxPriorityFeePerGas': w3.toWei(500.0, 'gwei'),
+                'chainId': 3,
+        }
+        signed_tx = w3.eth.account.signTransaction(tx, config.faucet_key)
+        try:
+            tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction).hex()
+        except:
+            now = datetime.now().astimezone(timezone(timedelta(hours=-8))).strftime('%m-%d %H:%M')
+            logging.info(f'{now}:Failed Transaction:{email}:{ip}:{to_address}')
+            return 0
+        return tx_hash
+
+    def get(self):
+        if not 'oauth_token' in session:
+        # Redirect to the identity provider if no token.
+        # Provider returns client to callback_url after auth
             google = OAuth2Session(config.client_id,
                     redirect_uri = config.callback_url,
                     scope = 'https://www.googleapis.com/auth/userinfo.email'
             )
             authorization_url, state = google.authorization_url(config.authorization_base_url)
 
-            # Identity provider returns URL and random "state" that must be echoed later
-            #   to prevent CSRF.
+            # Identity provider returns URL and random "state" to echo
+            # to prevent CSRF.
             session['oauth_state'] = state
             return redirect(authorization_url)
+
+        email = self.validate_oauth(session)
+        ip = self.validate_ip(request)
+
+        if (not email) or (not ip):
+            return redirect(url_for('logout'))
+
+        m = model.get_model()
+        wait_time = int(time.time()) - m.select(email, ip, "y")
+        if wait_time < 1209600:
+            return render_template('eth.html', email=email, wait=1)
+        else:
+            return render_template('eth.html', email=email)
 
     def post(self):
         """
         Accepts POST requests, and processes the form;
         Redirect to index when completed.
         """
-        def send_eth(to_address, send_amount):
-            w3 = Web3(Web3.HTTPProvider(config.node_url))
-            address1 = Web3.toChecksumAddress(config.faucet_address)
-            address2 = Web3.toChecksumAddress(to_address)
-            to_balance = w3.eth.get_balance(address2)
-            if to_balance > 10000000000000000000:
-                now = datetime.now().astimezone(timezone(timedelta(hours=-8))).strftime('%m-%d %H:%M')
-                logging.info(f'{now}:High Balance:{to_address}')
-                return 0
-            nonce = w3.eth.getTransactionCount(address1)
-            tx = {
-                    'nonce': nonce,
-                    'to': address2,
-                    'value': w3.toWei(send_amount, 'ether'),
-                    'gas': 21000,
-                    'maxFeePerGas': w3.toWei(1000.0, 'gwei'),
-                    'maxPriorityFeePerGas': w3.toWei(500.0, 'gwei'),
-                    'chainId': 3,
-            }
-            signed_tx = w3.eth.account.signTransaction(tx, config.faucet_key)
-            try:
-                tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction).hex()
-            except:
-                now = datetime.now().astimezone(timezone(timedelta(hours=-8))).strftime('%m-%d %H:%M')
-                logging.info(f'{now}:Failed Transaction: {to_address}')
-                tx_hash = 0
-            return tx_hash
+        if not 'oauth_token' in session:
+            return redirect(url_for('eth'))
 
-        if ('oauth_token' in session) and ('address' in request.form) and Web3.isAddress(request.form['address']):
-            # Insert based on form fields only if an OAuth2 token is present to ensure
-            #   values in all fields exist
-            google = OAuth2Session(config.client_id, token=session['oauth_token'])
-            try:
-                userinfo = google.get('https://www.googleapis.com/oauth2/v3/userinfo').json()
-            except:
-                return redirect(url_for('logout'))
+        email = self.validate_oauth(session)
+        ip = self.validate_ip(request)
 
-            email = userinfo['email']
+        if (not email) or (not ip):
+            return redirect(url_for('logout'))
 
-            if '@pdx.edu' in email:
-                send_amount = 10.0
-            else:
-                send_amount = 5.0
+        wallet = self.validate_wallet(request)
+        if not wallet:
+            return redirect(url_for('eth'))
 
-            if request.headers.getlist("X-Forwarded-For"):
-                ip = request.headers.getlist("X-Forwarded-For")[0]
-            else:
-                ip = request.remote_addr
+        m = model.get_model()
+        last = m.select(email,ip,wallet)
 
-            wallet = request.form['address']
+        send_amount = self.calculate_eth(email, ip, wallet, last)
 
-            m = model.get_model()
-            last = m.select(email,ip,wallet)
+        tx_hash = 0
+        if send_amount > 0:
+            tx_hash = self.send_eth(email, ip, wallet, send_amount)
+
+        if tx_hash:
             if last == 0:
                 m.insert(email, ip, wallet)
-                tx_hash = send_eth(wallet, send_amount)
-                return render_template('eth.html', email=userinfo['email'], tx_hash=tx_hash, wait=1)
-            elif int(time.time()) - last > 1209600:
-                m.update(email, ip, wallet)
-                tx_hash = send_eth(wallet, send_amount)
-                return render_template('eth.html', email=userinfo['email'], tx_hash=tx_hash, wait=1)
             else:
-                now = datetime.now().astimezone(timezone(timedelta(hours=-8))).strftime('%m-%d %H:%M')
-                logging.info(f'{now}:Rate Limit:{email}:{ip}:{wallet}')
-                return render_template('eth.html', email=userinfo['email'], wait=1)
-        else:
-            return redirect(url_for('eth'))
+                m.update(email, ip, wallet)
+
+        return render_template('eth.html', email=email, tx_hash=tx_hash, wait=1)
